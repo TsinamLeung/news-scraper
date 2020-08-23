c0527589f9cdee4882c81a9b5e50449493fc2e49
Zenam <liangzinan21@hotmail.com>
2020-08-23 13:15:40 +0800

    Modified Xinhuanet fetch way to dugkduckgo

--- Model/news_sanwah.js
+++ Model/news_sanwah.js
@@ -1,4 +1,4 @@
-const fetcher = require('./fetcher_news_common');
+const fetcher = require('./fetcher_news_via_search_engine');
 
 class news_sanwah extends fetcher {
   constructor(delay, pageLoaddelay) {
@@ -6,43 +6,9 @@ class news_sanwah extends fetcher {
         "_id": "news_sanwah",
         "startUrl": [],
         "selectors": [{
-          "id": "ele",
-          "type": "SelectorElementClick",
-          "parentSelectors": ["_root"],
-          "selector": "div.news",
-          "multiple": false,
-          "delay": "500",
-          "clickElementSelector": "a.next",
-          "clickType": "clickMore",
-          "discardInitialElements": "do-not-discard",
-          "clickElementUniquenessType": "uniqueHTMLText"
-        }, {
-          "id": "title_search",
-          "type": "SelectorText",
-          "parentSelectors": ["ele"],
-          "selector": "h2 a",
-          "multiple": false,
-          "regex": "",
-          "delay": 0
-        }, {
-          "id": "date_search",
-          "type": "SelectorText",
-          "parentSelectors": ["ele"],
-          "selector": "span",
-          "multiple": false,
-          "regex": "",
-          "delay": 0
-        }, {
-          "id": "link",
-          "type": "SelectorLink",
-          "parentSelectors": ["ele"],
-          "selector": "h2 a",
-          "multiple": true,
-          "delay": 0
-        }, {
           "id": "title",
           "type": "SelectorText",
-          "parentSelectors": ["link"],
+          "parentSelectors": ["_root"],
           "selector": "div.h-title,div#ArticleTit,div#Title",
           "multiple": false,
           "regex": "",
@@ -50,7 +16,7 @@ class news_sanwah extends fetcher {
         }, {
           "id": "content",
           "type": "SelectorText",
-          "parentSelectors": ["link"],
+          "parentSelectors": ["_root"],
           "selector": "div.mainCon,div#p-detail,div.content,div#Content",
           "multiple": false,
           "regex": "",
@@ -58,7 +24,7 @@ class news_sanwah extends fetcher {
         }, {
           "id": "date",
           "type": "SelectorText",
-          "parentSelectors": ["link"],
+          "parentSelectors": ["_root"],
           "selector": "div.laiyuan,span.h-time",
           "multiple": false,
           "regex": "",
@@ -68,11 +34,11 @@ class news_sanwah extends fetcher {
       delay,
       pageLoaddelay,
       'news_xinhua',
-      'CN')
-      this.updateStartURL = '新華網/news xinhua'
+      'CN',
+      'xinhuanet.com',
+      'duckduckgo')
+      this.description = '新華網/news xinhua'
   }
-  setKeyword(keyword) {
-    this.updateStartURL("http://so.news.cn/#search/0/" + encodeURI(keyword) + "/1/")
-  }
+
 }
 module.exports = news_sanwah;
