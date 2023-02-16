3f4ee2e648dae6baffeb9c9814a55c39500ae038
chinam <liangzinan21@hotmail.com>
2023-02-16 17:18:20 +0800

    [fix] 1. update.bat ,cannot install npm package

--- update.bat
+++ update.bat
@@ -1,5 +1,9 @@
 echo off
 echo 'Updating'
 git pull
+echo 'setting git to replace git to https'
+git config --global url."https://".insteadOf git:/
 npm install
+git config --global --unset url."https://".insteadOf git:/
+echo 'restoring git settings'
 pause
